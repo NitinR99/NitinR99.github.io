323e4b59c3aeac31fe75ecb7805658a1a1a69c65
NitinR99 <rames11a@uwindsor.ca>
2021-08-28 22:41:11 -0400

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ import 'rsuite/lib/styles/index.less';
 function App() {
   return (
 
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
     <NavigBar /><br/><br/>
       {/* switch allows switching which components render.  */}
 
       <Switch>
         {/* route allows you to render by url path */}
         <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={AboutMe} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/contact" component={Contact} />
+        <Route path="/about" component={AboutMe} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
         <Route  component={NotFound} />
       </Switch>
 
